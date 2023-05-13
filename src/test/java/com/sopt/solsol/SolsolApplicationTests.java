c88dfb0657f8e21c1806b8f6552373c299a45400
jinchiim <nellroll@naver.com>
2023-05-13 15:47:53 +0900

    initial commit

---
+++ src/test/java/com/sopt/solsol/SolsolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.solsol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolsolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
